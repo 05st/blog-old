c2304890327ca6e728c1dd2be96e4f796c075cdb
Sami <stimsina@protonmail.com>
2021-04-22 21:53:10 -0500

    Merge branch 'master' of https://github.com/05st/personal-website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import avatar from './res/avatar.png';
+import github from './res/github.png';
+
+function NavLink(props) {
+  return (
+    <a class="group block text-xl" href={props.link} target={props.newtab ? "_blank" : ""} rel="noreferrer">
+      {props.children}
+      {props.hover && <div class="w-full h-1/6 bg-black transition duration-100 opacity-0 group-hover:opacity-100"/>}
+    </a>
+  );
+}
+
+function Brand() {
+  return <h1 class="font-bold text-xl">SAMI TIMSINA</h1>;
+}
+
+function TopBar() {
+  return (
+    <div class="z-20 fixed w-full flex p-4 space-x-6 bg-gray-50 shadow text-center">
+      <Brand/>
+      <NavLink link="" hover={true}>Home</NavLink>
+      <NavLink link="" hover={true}>About</NavLink>
+      <NavLink link="https://github.com/05st" newtab={true}>
+        <img class="transition duration-100 opacity-50 hover:opacity-100" src={github} alt="GitHub"/>
+      </NavLink>
+    </div>
+  );
+}
+
+function LandingPage() {
+  return (
+    <div class="flex w-full min-h-screen grid grid-rows-3 bg-gradient-to-r from-blue-400 via-gray-100 to-gray-200">
+      <div class="col-span-3 mx-auto mt-36 w-96 h-96 p-4 bg-gray-50 rounded-full shadow-2xl transition duration-200 transform hover:scale-105">
+        <img class="rounded-full" src={avatar} alt="Avatar"/>
+      </div>
+      <div class="col-span-3 h-16 mx-auto mt-24 p-4 bg-gray-50 rounded-3xl shadow-inner">
+        <h1 class="text-xl text-center">Lorem <span class="font-bold">ipsum</span> dolor sit amet, consectetur adipiscing elit.</h1>
+        <p class="font-bold opacity-50 text-center mt-16">KEEP SCROLLING<br/>↓</p>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (<>
+    <TopBar/>
+    <LandingPage/>
+    <div class="w-full h-36 bg-gray-200">
+      <h1>Bonjour</h1>
+    </div>
+  </>);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
