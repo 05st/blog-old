f1f5be2318e6c4b4ed0b9b2381c1a1afa368a263
05st <05st@users.noreply.github.com>
2021-07-17 11:45:46 -0500

    Database querying

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyA7LvSSHQf9MkGQwHAKNzm_B1EfVZu8hgU",
+  authDomain: "blog-database-72722.firebaseapp.com",
+  projectId: "blog-database-72722",
+  storageBucket: "blog-database-72722.appspot.com",
+  messagingSenderId: "938125379165",
+  appId: "1:938125379165:web:983b7e6ebb0ba1e9786fb4",
+  measurementId: "G-1M4T5GXLF8"
+};
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+async function getPosts() {
+  let posts = [];
+  await db.collection("posts").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => posts[doc.id] = doc.data())
+  });
+  console.log(posts);
+  return posts;
+}
+
+let VIEWING = -1;
 
 function Topbar() {
   return (
@@ -21,7 +48,7 @@ function PostListing(props) {
   return (
     <div class="w-full flex p-2 shadow">
       <div class="items-center">
-        <button class="font-bold underline">{props.title}</button>
+        <button onClick={() => VIEWING = props.id} class="font-bold underline">{props.title}</button>
         <p class="text-sm">{props.date}</p>
       </div>
       <p class="pl-4">{props.desc}</p>
@@ -29,28 +56,24 @@ function PostListing(props) {
   )
 }
 
-var listdata = [{
-    title: "testing",
-    date: "2021-07-16",
-    desc: "testing description"
-  }, {
-    title: "testing2",
-    date: "2021-07-15",
-    desc: "testing desc2"
-  }
-];
-
 function PostList(props) {
   return (
     <div class="w-full content-center p-6 space-y-6 relative h-auto top-12">
-      {props.data.map((p, i) => <PostListing title={p.title} date={p.date} desc={p.desc}/>)}
+      {props.data.map((p, i) => <PostListing title={p.title} date={p.date} desc={p.description} id={i}/>)}
     </div>
   );
 }
 
+function Post(props) {
+
+}
+
 function App() {
+  const [posts, setPosts] = useState(0);
+  getPosts().then((q) => setPosts(q));
   return (<>
     <Topbar />
+    {posts && <PostList data={posts}/>}
   </>);
 }
 
