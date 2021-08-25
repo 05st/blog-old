71a1ce3fed0850cc05d10d5efbe930668b3a1dea
05st <05st@users.noreply.github.com>
2021-08-24 22:39:59 -0500

    LaTeX rendering support

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 
-import Markdown from "react-markdown";
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
 import {okaidia as syntaxTheme} from "react-syntax-highlighter/dist/cjs/styles/prism";
 import gfm from "remark-gfm";
+import Markdown from "react-markdown";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import remarkMath from "remark-math";
+import rehypeKatex from  "rehype-katex";
+import rehypeMathjax from "rehype-mathjax";
+import rehypeRaw from "rehype-raw";
 
 import firebase from "../../config/firebase-config";
 import "firebase/firestore";
@@ -39,12 +45,14 @@ export default function Post(props) {
         <meta property="og:type" content="website"/>
         <meta property="og:site_name" content="blog.stimsina.com"/>
         <meta property="og:description" content={props.description}/>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css" integrity="sha384-RZU/ijkSsFbcmivfdRBQDtwuwVqK7GMOw6IMvKyeWL2K5UAlyp6WonmB8m7Jd0Hn" crossorigin="anonymous"/>
       </Head>
       <div className="relative flex flex-col w-full p-6 top-16 items-center space-y-6">
         <p className="text-sm text-gray-300">{props.title} ({props.date})</p>
         { markdown ?
           <div className="markdown-body lg:w-1/2 w-full">
-            <Markdown remarkPlugins={[gfm]} components={{code: CodeBlock}} linkTarget="_blank">{markdown}</Markdown>
+            <Markdown remarkPlugins={[gfm, remarkMath, remarkRehype]} rehypePlugins={[rehypeKatex]} components={{code: CodeBlock}} linkTarget="_blank">{markdown}</Markdown>
           </div>
           : <p className="font-bold text-lg">Loading</p> }
       </div>
